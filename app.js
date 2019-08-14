4bb46814b71941117ae6eced5fc7ac160d17fd92
Daniel Pape <danielwpape@gmail.com>
2019-08-14 09:38:47 +0100

    Added dev and prod environments

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const userRouter = require('./routes/userRoutes');
 
 // 1) MIDDLEWARE
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');

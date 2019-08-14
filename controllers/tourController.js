4cdf791f7144743eeec28a8e8baffb019714cbae
Daniel Pape <danielwpape@gmail.com>
2019-08-14 08:58:42 +0100

    Created Check ID Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour ID is ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -23,11 +34,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
 
-  //   if (id > tours.length)
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -37,11 +43,6 @@ exports.getTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  //   if (id > tours.length)
-  if (req.id.params > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -69,11 +70,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  //   if (id > tours.length)
-  if (req.id.params * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null
